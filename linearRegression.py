5aefe403b87ec3001ad8c62f72456e15f83e8b79
rabbottz <42052011+rabbottz@users.noreply.github.com>
2019-05-23 13:54:55 -0700

    Add files via upload

---
+++ linearRegression.py
@@ -0,0 +1,60 @@
+'''
+Name: Andrew Abbott
+Username: aabbott
+Computes the linear regression line of a given data set using matrix algebra
+Usage: python linearRegression.py
+
+'''
+
+import numpy
+import csv
+import matplotlib.pyplot as plt
+
+
+def gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    #make the matrix
+    x = points[:, 0]
+    y = points[:, 1]
+    b_gradient = sum(-(2/N) * (y - ((m_current * x) + b_current)))
+    m_gradient = sum(-(2/N) * x * (y - ((m_current * x) + b_current)))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent(points, starting_b, starting_m, learning_rate, num_iter):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iter):
+        b, m = gradient(b, m, points, learning_rate)
+    return [b, m]
+
+def plot_results(points,m,b):
+    x_training = [point[0] for point in points]
+    y_training = [point[1] for point in points]
+    y_prediction = [m*x + b for x in x_training]
+    plt.plot(x_training,y_prediction,color='r')
+    plt.scatter(x_training,y_training,color='b')
+    plt.title("Vectorized Linear Regression")
+    plt.show()
+
+def init():
+    #read file into list of x,y coordinates, one set of coords per line
+    reader = list(csv.reader(open("input.csv", "rb"), delimiter=','))
+    points = numpy.array(reader).astype('float')
+
+    #set the initial parameters
+    learning_rate = 0.0001  #step size
+    b = 0 # initial y-intercept guess
+    m = 0 # initial slope guess
+    num_iter = 1000 #number of iterations of gradient across all points
+    return points, learning_rate, b, m, num_iter
+
+def main():
+    points, learning_rate, b, m, num_iter = init()
+    [b, m] = gradient_descent(points, b, m, learning_rate, num_iter)
+    plot_results(points,m,b)
+
+main()
